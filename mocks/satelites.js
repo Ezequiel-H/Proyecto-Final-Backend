08eb85b581bfee84cfdb08b36c28831f618e7b0e
Ezequiel-H <horowitzeze@gmail.com>
2023-08-03 23:55:06 -0300

    fix: ndvi name

--- mocks/satelites.js
+++ mocks/satelites.js
@@ -3,5 +3,5 @@ export const mockAddSateliteData = (plots) =>
     humidity: Math.random(),
     color: Math.floor(Math.random() * 16777215).toString(16),
     frost: Math.random(),
-    NDVI: Math.random() > 0.5 ? Math.random() : -1 * Math.random(),
+    ndvi: Math.random() > 0.5 ? Math.random() : -1 * Math.random(),
   }));
