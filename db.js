f23793655a41123adb65d3a409d03fc40b5c4a5d
Ezequiel-H <horowitzeze@gmail.com>
2023-08-16 23:36:31 -0300

    feat: choose local or remote db

--- db.js
+++ db.js
@@ -3,9 +3,12 @@ import mongooseLib from "mongoose";
 export const mongoose = mongooseLib;
 
 export const initDatabase = () => {
-  mongoose.connect(
-    `mongodb+srv://${process.env.ATLAS_USER}:${process.env.ATLAS_PASSWORD}@cluster0.cbnf0w7.mongodb.net/?retryWrites=true&w=majority`
-  );
+  const connectionString =
+    process.env.DB == "atlas"
+      ? `mongodb+srv://${process.env.ATLAS_USER}:${process.env.ATLAS_PASSWORD}@cluster0.cbnf0w7.mongodb.net/?retryWrites=true&w=majority`
+      : "mongodb://127.0.0.1:27017/proyectoX";
+
+  mongoose.connect(connectionString);
   return mongoose;
 };
 
