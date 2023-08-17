111065ece5be9554a9f633a243a779512283db2a
Ezequiel-H <horowitzeze@gmail.com>
2023-08-16 23:32:58 -0300

    feat: using atlas db

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
-import { initDatabase } from './db.js';
-import app from './app_builder.js';
+import { initDatabase } from "./db.js";
+import app from "./app_builder.js";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 initDatabase();
 
--- db.js
+++ db.js
@@ -3,7 +3,9 @@ import mongooseLib from "mongoose";
 export const mongoose = mongooseLib;
 
 export const initDatabase = () => {
-  mongoose.connect("mongodb://127.0.0.1:27017/proyectoX");
+  mongoose.connect(
+    `mongodb+srv://${process.env.ATLAS_USER}:${process.env.ATLAS_PASSWORD}@cluster0.cbnf0w7.mongodb.net/?retryWrites=true&w=majority`
+  );
   return mongoose;
 };
 
